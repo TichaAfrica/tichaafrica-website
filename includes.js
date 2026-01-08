fecf739e1c818c4105aa5d70045c03b552830ff8
TichaAfrica <tichaafrica@gmail.com>
2026-01-08 17:31:18 +0100

    Update includes.js

--- includes.js
+++ includes.js
@@ -1,7 +1,22 @@
-fetch('partials/header.html')
-  .then(res => res.text())
-  .then(data => document.getElementById('site-header').innerHTML = data);
-
-fetch('partials/footer.html')
-  .then(res => res.text())
-  .then(data => document.getElementById('site-footer').innerHTML = data);
+function includeHTML() {
+  var z, i, elmnt, file, xhttp;
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    file = elmnt.getAttribute("w3-include-html");
+    if (file) {
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+          if (this.status == 200) { elmnt.innerHTML = this.responseText; }
+          if (this.status == 404) { elmnt.innerHTML = "Page not found."; }
+          elmnt.removeAttribute("w3-include-html");
+          includeHTML(); // recursive call for nested includes
+        }
+      }
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      return;
+    }
+  }
+}
